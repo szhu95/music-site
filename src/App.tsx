79793f6e77cbdccd27be1b4fd9181696affafd17
Stephen Hu <32474230+szhu95@users.noreply.github.com>
2020-04-16 14:14:40 -0400

    Fixing style of images

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,37 @@ function App() {
         <h1>STEVIE HU</h1>
         <div className='mixer-container'>
           <div className='mixer-link'>
-            <img src={turntable} alt='' className='mixer-image' />
-            <img src={vinyl} alt='' className='vinyl-image' />
-            ABOUT
-          </div>
-          <img src={mixer} alt='' className='mixer-image' />
-          <div className='mixer-link'>
-            <img src={turntable} alt='' className='mixer-image' />
+            <img src={turntable} alt='' className='turntable-image' />
             <img src={vinyl} alt='' className='vinyl-image' />
             MIXES
           </div>
+          <img src={mixer} alt='' className='mixer-image' />
+          <div className='mixer-link'>
+            <img src={turntable} alt='' className='turntable-image' />
+            <img src={vinyl} alt='' className='vinyl-image' />
+            CONTACT
+          </div>
         </div>
       </header>
-      <div>Mixes</div>
+      <div>
+        Mixes
+        <iframe
+          width='100%'
+          height='300'
+          scrolling='no'
+          frameBorder='no'
+          allow='autoplay'
+          src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/788603386&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+        ></iframe>
+        <iframe
+          width='100%'
+          height='300'
+          scrolling='no'
+          frameBorder='no'
+          allow='autoplay'
+          src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/756416887&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true'
+        ></iframe>
+      </div>
     </div>
   );
 }
